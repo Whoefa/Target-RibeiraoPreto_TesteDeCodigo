ed1379513d1c016ea179b391c293794c686976cf
Emanoel Faria <69714572+Whoefa@users.noreply.github.com>
2024-09-06 16:07:03 -0300

    Desafio Verificar a quantidade de "A"

---
+++ VerificarQuantidadeA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class VerificarA
+{
+    public static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Informe um texto para verificar a quantidade de vogal A:");
+        string texto = Console.ReadLine();
+
+       
+        int contadorA = 0;
+
+        // Como eu nao sei o tamanho do texto vou usar um foreach para pecorrer
+        foreach (char letra in texto)
+        {
+            
+            if (letra == 'A' || letra == 'a')
+            {
+                contadorA++;
+            }
+        }
+
+        
+            Console.WriteLine($"A vogal 'a' aparece {contadorA} vezes.");
+      
+            
+        
+    }
+}
